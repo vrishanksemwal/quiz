4016a5264779cae53a500a20423b706d81c40889
vrishanksemwal <80450599+vrishanksemwal@users.noreply.github.com>
2021-09-20 01:07:26 -0700

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "SHAHRUKH";
+const accessCode2 = "SACHIN";
+const accessCode3 = "KATRINA";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("H H S A K R H U ", 100,50)
+    fill("lightblue")
+    text("Hint: Vrishank's favourite actor !!", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("N H I A C S", 700,150)
+    fill("lightblue")
+    text("Hint: Vrishank's favourite cricketer !!", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("A A K R T I N", 100,250)
+    fill("lightblue")
+    text("Hint: Vrishank's favourite actress !!", 100,270)
+
+}
